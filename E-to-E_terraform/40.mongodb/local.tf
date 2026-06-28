58a14cdab047b25ba4bb9b4c07a3dafee2833cfc
SUdhakar20000 <mandahemasudhakar@gmail.com>
2026-06-28 18:17:41 +0530

    first commit

--- E-to-E_terraform/40.mongodb/local.tf
+++ E-to-E_terraform/40.mongodb/local.tf
@@ -1,7 +1,7 @@
 locals {
     common_name = "${var.project}-${var.env}"
-    bastion_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
-    frounttir_subnet_id = split(",", data.aws_ssm_parameter.dbtire_subnet_ids.value)[0]
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    dbtire_subnet_id = split(",", data.aws_ssm_parameter.dbtire_subnet_ids.value)[0]
     common_tags = {
         Project = "var.project"
         Env = "var.env"
