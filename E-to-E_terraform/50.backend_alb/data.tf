a4080de27077d44ff22797974a62c70e0b7e13a2
Sudhakar20000 <mandahemasudhakar@gmail.com>
2026-07-01 16:38:12 +0530

    backend_alb

--- E-to-E_terraform/50.backend_alb/data.tf
+++ E-to-E_terraform/50.backend_alb/data.tf
@@ -2,6 +2,6 @@ data "aws_ssm_parameter" "backend_alb_sg_id" {
     name = "/${var.project}/${var.env}/backend_alb_sg_id"
 }
 
-data "aws_ssm_parameter" "private_subnet_ids" {
+data "aws_ssm_parameter" "apptire_subnet_ids" {
     name = "/${var.project}/${var.env}/apptire_subnet_ids"
 }
